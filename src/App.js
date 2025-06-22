ea60932dbda5b788349cae868ce43e7a0c3ba3f2
anand25may <anand.gupta.blg@gmail.com>
2025-06-22 17:34:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 Welcome to My DevOps Portfolio</h1>
+      <p>Deployed via Docker, AKS, and Azure DevOps CI/CD 🚢</p>
+    </div>
+  );
+}
+
+export default App;
